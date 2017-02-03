bd8f1366fadb13f594039b81a113a92cd5282e02
pellea <adrien.pellegrini@gmail.com>
2017-02-03 16:02:09 +0100

    switch to oidc-client-fetch

--- src/helpers/createUserManager.js
+++ src/helpers/createUserManager.js
@@ -1,4 +1,4 @@
-import { UserManager } from 'oidc-client';
+import { UserManager } from 'oidc-client-fetch';
 
 export default function createUserManager(config) {
   return new UserManager(config);
